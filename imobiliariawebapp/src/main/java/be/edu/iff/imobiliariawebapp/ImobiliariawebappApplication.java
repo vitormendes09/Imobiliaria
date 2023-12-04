60a72bbeae6991663e4917c3f14489d51dc63325
Vitor Silva Mendes <93722266+vitormendes09@users.noreply.github.com>
2023-12-04 14:23:42 -0300

    criando estrutura do projeto e instalando o spring boot

---
+++ imobiliariawebapp/src/main/java/be/edu/iff/imobiliariawebapp/ImobiliariawebappApplication.java
@@ -0,0 +1,13 @@
+package be.edu.iff.imobiliariawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobiliariawebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobiliariawebappApplication.class, args);
+	}
+
+}
---
+++ imobiliariawebapp/src/test/java/be/edu/iff/imobiliariawebapp/ImobiliariawebappApplicationTests.java
@@ -0,0 +1,13 @@
+package be.edu.iff.imobiliariawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobiliariawebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
